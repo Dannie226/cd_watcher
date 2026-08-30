26267b13f971dd0594f812fe37485026c304fc6e
Daniel Quednow <danquednow@gmail.com>
2026-08-30 18:10:07 -0400

    Change around unpacking slightly
    I wanted to be able to enumerate the bundles in the sent email
    so instead of repeating the directory read work, I just passed
    in the read values.

--- internal/unpack/unpack.go
+++ internal/unpack/unpack.go
@@ -4,33 +4,31 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"log/slog"
 	"os"
 	"os/exec"
 	"path/filepath"
 
+	"github.com/Dannie226/cd_watcher/internal/queries"
 	"github.com/jackc/pgx/v5"
 )
 
-var ErrNoBundles error = fmt.Errorf("No bundles found for deploy")
-
-func UnpackBundles(uploadDir, releaseDir, unpackScript string, uploadConn *pgx.Conn) (string, error) {
-	entries, err := os.ReadDir(uploadDir)
+func UnpackBundles(uploadDir, releaseDir, unpackScript string, bundles []os.DirEntry, uploadConn *pgx.Conn) (string, error) {
+	id, err := queries.GetNextVersionID(uploadConn)
 
 	if err != nil {
-		return "", fmt.Errorf("Failed to read upload directory: %w", err)
-	}
-
-	if len(entries) == 0 {
-		return "", ErrNoBundles
+		return "", fmt.Errorf("Failed to get next release id: %w", err)
 	}
 
 	var relName string
 
-	for _, e := range entries {
-		randBytes := [16]byte{0}
+	for _, e := range bundles {
+		randBytes := [24]byte{0}
 		rand.Read(randBytes[:])
 
 		relName = base64.RawURLEncoding.EncodeToString(randBytes[:])
+
+		slog.Info("Unpacking bundle", "bundle name", e.Name(), "release folder", relName)
 
 		relDir := filepath.Join(releaseDir, relName, "")
 		entryName := filepath.Join(uploadDir, e.Name())
@@ -66,6 +64,17 @@ func UnpackBundles(uploadDir, releaseDir, unpackScript string, uploadConn *pgx.C
 		if err := cmd.Run(); err != nil {
 			return "", fmt.Errorf("Failed to run unpack script on \"%s\": %w", e.Name(), err)
 		}
+
+		queries.InsertNewVersion(uploadConn, queries.VersionInfo{
+			ID:         id,
+			FolderName: relName,
+		})
+
+		if err != nil {
+			return "", fmt.Errorf("Failed to insert release into database: %w", err)
+		}
+
+		id++
 
 		err = os.Remove(entryName)
 
